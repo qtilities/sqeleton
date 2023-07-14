709e5e7f5218e082a31dadef94c7a896987be321
redtide <redtid3@gmail.com>
2023-07-14 11:08:58 +0200

    Added missing license headers

--- src/dialogprefs.cpp
+++ src/dialogprefs.cpp
@@ -1,3 +1,26 @@
+/*
+    MIT License
+
+    Copyright (c) 2021-2023 Andrea Zanellato <redtid3@gmail.com>
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to
+    deal in the Software without restriction, including without limitation the
+    rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+    sell copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
+*/
 #include "dialogprefs.hpp"
 #include "ui_dialogprefs.h"
 #include "application.hpp"
@@ -51,6 +74,7 @@ void Qtilities::DialogPrefs::loadSettings()
 void Qtilities::DialogPrefs::accept()
 {
     Settings &settings = static_cast<Application *>(qApp)->settings();
+
     settings.setBackgroundColor(
         ui->lblColorBg->palette().color(QPalette::Window));
     settings.setForegroundColor(
