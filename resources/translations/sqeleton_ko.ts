9d812fca02ed4a3d2f4b402f675e606842da9dcb
JungHee Lee <36289819+MarongHappy@users.noreply.github.com>
2023-08-29 14:05:23 +0900

    Create sqeleton_ko.ts

---
+++ resources/translations/sqeleton_ko.ts
@@ -0,0 +1,108 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko">
+<context>
+    <name>Qtilities::DialogAbout</name>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="27"/>
+        <source>Information</source>
+        <translation type="unfinished">정보</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="62"/>
+        <source>qrc:/about.html</source>
+        <translation type="unfinished">qrc:/about.html</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="81"/>
+        <source>Thanks</source>
+        <translation type="unfinished">감사한 분들</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="119"/>
+        <source>qrc:/thanks.html</source>
+        <translation type="unfinished">qrc:/thanks.html</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="138"/>
+        <source>License</source>
+        <translation type="unfinished">라이선스</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="176"/>
+        <source>qrc:/license.html</source>
+        <translation type="unfinished">qrc:/license.html</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.cpp" line="60"/>
+        <source>About</source>
+        <translation type="unfinished">정보</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.cpp" line="55"/>
+        <source>Author</source>
+        <translation type="unfinished">작성자</translation>
+    </message>
+</context>
+<context>
+    <name>Qtilities::DialogPrefs</name>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="21"/>
+        <source>Colors</source>
+        <translation type="unfinished">색상</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="72"/>
+        <source>Background</source>
+        <translation type="unfinished">배경</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="82"/>
+        <source>Text</source>
+        <translation type="unfinished">텍스트</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.cpp" line="41"/>
+        <source>Preferences</source>
+        <translation type="unfinished">환경설정</translation>
+    </message>
+</context>
+<context>
+    <name>Qtilities::MainWindow</name>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="42"/>
+        <source>Hello!</source>
+        <translation type="unfinished">안녕하세요!</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="66"/>
+        <source>&amp;Edit</source>
+        <translation type="unfinished">편집(&amp;E)</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="72"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished">도움말(&amp;H)</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="78"/>
+        <source>&amp;File</source>
+        <translation type="unfinished">파일(&amp;F)</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="92"/>
+        <source>Pr&amp;eferences</source>
+        <translation type="unfinished">환경설정(&amp;E)</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="107"/>
+        <source>About</source>
+        <translation type="unfinished">정보</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="122"/>
+        <source>E&amp;xit</source>
+        <translation type="unfinished">종료(&amp;X)</translation>
+    </message>
+</context>
+</TS>
